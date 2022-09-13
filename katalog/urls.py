34c733619431aed649c51f1a8cbcfe9a1047dffa
rubentanoey <92169008+rubentanoey@users.noreply.github.com>
2022-09-13 23:31:29 +0700

    Completed PBP Assignment 2

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,1 +1,9 @@
 # TODO: Implement Routings Here
+from django.urls import path
+from katalog.views import show_catalog
+
+app_name = 'katalog'
+
+urlpatterns = [
+    path('', show_catalog, name='show_catalog'),
+]
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # TODO: Create your views here.
+from katalog.models import CatalogItem
+
+def show_catalog(request):
+    catalog = CatalogItem.objects.all()
+    return render(
+        request,
+        "katalog.html",
+        {
+            "name": "Ruben Tanoey",
+            "student_id": "2106752445",
+            "catalogs": catalog,
+        },
+    )
